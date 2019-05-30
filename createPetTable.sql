c4402949e02258bc6aedb5c57fe5b6fe2b45f140
rxy24 <robinxie24@gmail.com>
2019-05-30 08:39:00 -0700

    Revision
    Removed Rabbit, Dog, Cat subclasses and Donor, Adoptor subclasses. Merged FoodOrder tables into one table

--- createPetTable.sql
+++ createPetTable.sql
@@ -1,3 +1,24 @@
+create Table Customer_R1 (
+   Postalcode char(6) PRIMARY KEY,
+   City char(20),
+   Province char(20)
+);
+
+create Table Customer_R2 (
+   Name char(20) PRIMARY KEY,
+   Birthdate char(20)
+);
+
+create Table Customer (
+   customer_id char(10) PRIMARY KEY,
+   name char(20),
+   street char(20),
+   postalcode char(6),
+   FOREIGN KEY (Name) REFERENCES Customer_R2 (Name) ON DELETE CASCADE,
+   FOREIGN KEY (postalcode) REFERENCES Customer_R1 (Postalcode) ON DELETE CASCADE
+);
+
+
 create table Animal_R2(
     breed char(20) PRIMARY KEY,
     hypoallergenic char(5));
@@ -9,36 +30,21 @@ create table Animal(
     gender char(6),
     breed char(20),
     donation_date char(30),
-    customer_id char(20),
+    customer_id char(10),
     FOREIGN KEY (breed) REFERENCES Animal_R2 (breed),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
-
-create Table Dog(
-    animal_id char(10) PRIMARY KEY,
-    dewormed char(20),
-    FOREIGN KEY (animal_id) REFERENCES Animal (animal_id) ON DELETE CASCADE);
-
-create Table Cat(
-    animal_id char(10) PRIMARY KEY,
-    declawed char(20),
-    FOREIGN KEY (animal_id) REFERENCES Animal (animal_id) ON DELETE CASCADE);
-
-create Table Rabbit(
-    animal_id char(10) PRIMARY KEY,
-    overgrownteeth char(20),
-    FOREIGN KEY (animal_id) REFERENCES Animal (animal_id) ON DELETE CASCADE);
 
 create Table Room(
     room_id char(20) PRIMARY KEY,
     room_size integer);
 
 create Table Doctor(
-    doctor_id char(20) PRIMARY KEY,
+    doctor_id char(10) PRIMARY KEY,
     name char(20));
 
 create Table Doctor_R2(
-    doctor_id char(20),
+    doctor_id char(10),
     animal_id char(10) PRIMARY KEY,
     FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id) ON DELETE CASCADE,
     FOREIGN KEY (animal_id) REFERENCES Animal(animal_id) ON DELETE CASCADE);
@@ -48,19 +54,19 @@ create Table Health_Status_Check (
     vaccination char(20),
     allergies char(20),
     previous_diseases char(20),
-    doctor_id char(20),
+    doctor_id char(10),
     FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id) ON DELETE CASCADE);
 
 create Table Drugs_Prescribes(
     name char(20),
-    doctor_id char(20),
+    doctor_id char(10),
     dosage integer,
     PRIMARY KEY (name, doctor_id),
     FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id) ON DELETE CASCADE);
 
 create Table Checks (
     health_id char(20),
-    doctor_id char(20),
+    doctor_id char(10),
     animal_id char(10),
     PRIMARY KEY (health_id,doctor_id, animal_id),
     FOREIGN KEY (health_id) REFERENCES Health_Status_Check(health_id) ON DELETE CASCADE,
@@ -73,3 +79,37 @@ create Table Stay_In (
     PRIMARY KEY (room_id, animal_id),
     FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE,
     FOREIGN KEY (animal_id) REFERENCES Animal(animal_id) ON DELETE CASCADE);
+
+CREATE TABLE Administrator(
+    AdminID char(10) PRIMARY KEY,
+    Name char(20),
+    Animal_ID char(10),
+    FOREIGN KEY (Animal_id) REFERENCES Animal (animal_id)
+ );
+
+CREATE TABLE Registers(
+    AdminID char(10),
+    Animal_ID char(10),
+    Date_reg char(20),
+    PRIMARY KEY(AdminID, Animal_ID),
+    FOREIGN KEY (AdminID) REFERENCES Administrator (AdminID) ON DELETE CASCADE,
+    FOREIGN KEY (Animal_id) REFERENCES Animal (animal_id) ON DELETE CASCADE
+);
+
+CREATE TABLE FoodOrders(
+    InvoiceNo char(5),
+    Type_Food char(10),
+    Name char(10),
+    Cost char(10),
+    AdminID char(10),
+    Nutrition char(10),
+    PRIMARY KEY (InvoiceNo, Type_Food),
+    FOREIGN KEY (AdminID) REFERENCES Administrator (AdminID) ON DELETE CASCADE
+);
+ 
+CREATE TABLE Viewing_Sets(
+    ViewingID char(10) PRIMARY KEY,
+    CustomerID char(10),
+    Date_view char(10),
+    FOREIGN KEY (CustomerID) REFERENCES Customer (customer_id) ON DELETE CASCADE
+);
--- insertPetData.sql
+++ insertPetData.sql
@@ -1,4 +1,4 @@
-/*Insert data into Animal_R2 table*/
+/*Animal_R2 table*/
 
 insert into Animal_R2 values
 ('Corgi', 'No');
@@ -15,21 +15,53 @@ insert into Animal_R2 values
 insert into Animal_R2 values
 ('Bobtail', 'Yes');
 
-/*Insert data into Customer Table*/
+/*Customer_R1 Table*/
+insert into Customer_R1 values
+('V6P1V7','Vancouver', 'BC');
+
+insert into Customer_R1 values
+('V7J1Y2','Vancouver', 'BC');
+
+insert into Customer_R1 values
+('V5P1B7','Abbotsford', 'BC');
+
+insert into Customer_R1 values
+('V1P5V7','Vancouver', 'BC');
+
+insert into Customer_R1 values
+('V0P1V6','Calgary', 'AB');
+
+/*Customer_R2 Table*/
+insert into Customer_R2 values
+('Jacob','1990-03-05');
+
+insert into Customer_R2 values
+('May','1970-10-20');
+
+insert into Customer_R2 values
+('John','1962-03-25');
+
+insert into Customer_R2 values
+('Kelly','1988-04-15');
+
+insert into Customer_R2 values
+('Rick','1980-07-12');
+
+/*Customer Table*/
 insert into Customer values
-('1','Jacob', 'Oak Street', 'V6P1V7', 'Vancouver', 'British Columbia', '1990-03-05');
+('1','Jacob', 'Oak Street', 'V6P1V7');
 
 insert into Customer values
-('2','May', 'Fleming Street', 'V7J1Y2', 'Vancouver', 'British Columbia', '1970-10-20');
+('2','May', 'Fleming Street', 'V7J1Y2');
 
 insert into Customer values
-('3','John', 'Maple Street', 'V5P1B7', 'Abbotsford', 'British Columbia', '1962-03-25');
+('3','John', 'Maple Street', 'V5P1B7');
 
 insert into Customer values
-('4','Kelly', 'Acorn Street', 'V1P5V7', 'Vancouver', 'British Columbia', '1988-04-15');
+('4','Kelly', 'Acorn Street', 'V1P5V7');
 
 insert into Customer values
-('5','Rick', 'Arc Street', 'V0P1V6', 'Calgary', 'Alberta', '1980-07-12');
+('5','Rick', 'Arc Street', 'V0P1V6');
 /*Insert data into Animal table*/
 
 insert into Animal values
@@ -47,91 +79,7 @@ insert into Animal values
 insert into Animal values
 ('5', 'Luna', 3, 'Female', 'Schnauzer', 'May 3, 2019','2');
 
-insert into Animal values
-('6', 'Happy', 3, 'Male', 'Siamese', 'May 1, 2019','2');
-
-insert into Animal values
-('7', 'Toast', 5, 'Female', 'English Lop', 'May 2, 2019','2');
-
-insert into Animal values
-('8', 'Bud', 9, 'Male', 'Siamese', 'May 3, 2019','2');
-
-insert into Animal values
-('9', 'Bouncer', 8, 'Male', 'English Lop', 'May 9, 2019','2');
-
-insert into Animal values
-('10', 'Teddy', 8, 'Male', 'Corgi', 'May 25, 2019','2');
-
-insert into Animal values
-('11', 'Lily', 8, 'Male', 'Schnauzer', 'May 21, 2019','2');
-
-insert into Animal values
-('12', 'Charlie', 8, 'Male', 'Siamese', 'May 21, 2019','2');
-
-insert into Animal values
-('13', 'Tabby', 8, 'Male', 'Siamese', 'May 23, 2019','2');
-
-insert into Animal values
-('14', 'Carrot', 8, 'Female', 'English Lop', 'May 23, 2019','2');
-
-insert into Animal values
-('15', 'Peanut', 8, 'Male', 'English Lop', 'May 23, 2019','2');
-
-insert into Animal values
-('16', 'Berry', 8, 'Female', 'English Lop', 'May 23, 2019','1');
-
-/*Insert data into Dog table*/
-
-insert into Dog values
-('1', 'False');
-
-insert into Dog values
-('3', 'True');
-
-insert into Dog values
-('5', 'True');
-
-insert into Dog values
-('10', 'False');
-
-insert into Dog values
-('11', 'True');
-
-/*Insert data into Cat table*/
-
-insert into Cat values
-('2', 'False');
-
-insert into Cat values
-('6', 'True');
-
-insert into Cat values
-('8', 'True');
-
-insert into Cat values
-('12', 'False');
-
-insert into Cat values
-('13', 'True');
-
-/*Insert data into Rabbit table*/
-
-insert into Rabbit values
-('4', 'False');
-
-insert into Rabbit values
-('7', 'True');
-
-insert into Rabbit values
-('14', 'True');
-
-insert into Rabbit values
-('15', 'False');
-
-insert into Rabbit values
-('16', 'True');
-
-/*Insert data into Room table*/
+/*Room table*/
 insert into Room values
 ('1', 5);
 
@@ -157,13 +105,7 @@ insert into Doctor values
 insert into Doctor values
 ('3', 'Bear');
 
-insert into Doctor values
-('4', 'Brooklyn');
-
-insert into Doctor values
-('5', 'Chelsea');
-
-/*Insert data into Doctor_R2 table*/
+/*Doctor_R2 table*/
 insert into Doctor_R2 values
 ('1', '1');
 
@@ -173,13 +115,7 @@ insert into Doctor_R2 values
 insert into Doctor_R2 values
 ('3', '3');
 
-insert into Doctor_R2 values
-('1', '4');
-
-insert into Doctor_R2 values
-('2', '5');
-
-/*Insert data into Health_Status_Check table*/
+/*Health_Status_Check table*/
 insert into Health_Status_Check values
 ('1', 'Yes', 'No', 'Dry Eyes', '1');
 
@@ -195,7 +131,7 @@ insert into Health_Status_Check values
 insert into Health_Status_Check values
 ('5', 'No', 'No', 'Not Applicable', '3');
 
-/*Insert data into Drugs_Prescribes table*/
+/*Drugs_Prescribes table*/
 insert into Drugs_Prescribes values
 ('Acepromazine', '5', 2);
 
@@ -205,13 +141,7 @@ insert into Drugs_Prescribes values
 insert into Drugs_Prescribes values
 ('Capstar', '2', 5);
 
-insert into Drugs_Prescribes values
-('Doxycycline', '5', 2);
-
-insert into Drugs_Prescribes values
-('Ephedrine', '1', 20);
-
-/*Insert data into Checks table*/
+/*Checks table*/
 insert into Checks values
 ('1', '1', '1');
 
@@ -227,20 +157,65 @@ insert into Checks values
 insert into Checks values
 ('5', '5', '5');
 
-/*Insert data into Stay_In table*/
+/*Stay_In table*/
 insert into Stay_In values
-('1', '5');
+('1', '1');
 
 insert into Stay_In values
-('1', '6');
+('1', '2');
 
 insert into Stay_In values
-('2', '9');
+('2', '3');
 
 insert into Stay_In values
-('2', '5');
+('2', '4');
 
 insert into Stay_In values
-('3', '7');
+('3', '5');
+
+/*Admin Data*/
+INSERT INTO Administrator VALUES('1','Chris', '1');
+
+INSERT INTO Administrator VALUES('2','Gary', '5');
+
+INSERT INTO Administrator VALUES('3','Lenny', '4');
+
+INSERT INTO Administrator VALUES('4','Annie', '3');
+
+INSERT INTO Administrator VALUES('5','Dalena', '2');
+
+/*Registration Data*/
+INSERT INTO Registers
+VALUES('1','1', '2019-05-20');
+
+INSERT INTO Registers
+VALUES('2','5','2019-05-21');
+
+INSERT INTO Registers
+VALUES('3','4', '2019-05-22');
+
+INSERT INTO Registers
+VALUES('4','2', '2019-05-23');
+
+INSERT INTO Registers
+VALUES('5','3', '2019-05-24');
+
+
+/*Food Orders*/
+INSERT INTO FoodOrders VALUES('1', 'Fish', 'Brand A', '10','1','Protein');
+
+INSERT INTO FoodOrders VALUES('2', 'Fish', 'Brand B', '20.20','1', 'Omega C');
+
+INSERT INTO FoodOrders VALUES('3', 'Fish', 'Brand C', '30.30','2', 'Carbs');
+
+
+/*Viewing Set*/
+INSERT INTO Viewing_Sets VALUES('1','1', '2019-05-25');
+
+INSERT INTO Viewing_Sets VALUES('2','2', '2019-05-20');
+
+INSERT INTO Viewing_Sets VALUES('3','3', '2019-05-22');
+
+
 
 commit work;
